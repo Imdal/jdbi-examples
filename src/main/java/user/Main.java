dba569f42f26a90eea88742dc708131fef58d3a2
imdal <imdal1220@gmail.com>
2020-04-12 20:37:58 +0200

    fixed errors

--- src/main/java/user/Main.java
+++ src/main/java/user/Main.java
@@ -1,11 +1,8 @@
 package user;
 
-import ex9.LegoSet;
-import ex9.LegoSetDao;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
-import org.jdbi.v3.sqlobject.customizer.Bind;
 
 import java.time.LocalDate;
 
@@ -16,11 +13,11 @@ public class Main {
         try (Handle handle = jdbi.open()) {
             UserDao dao = handle.attach(UserDao.class);
             dao.createTable();
-            User user = User.builder().id(6900345).username("imdal").password("password").name("Imre Dalma").email("imdal@mailbox.unideb.hu").dob(LocalDate.parse("1999-12-20")).gender(User.Gender.FEMALE).enabled(true);
+            User user = User.builder().id((long) 6900345).username("imdal").password("password").name("Imre Dalma").email("imdal@mailbox.unideb.hu").dob(LocalDate.parse("1999-12-20")).gender(User.Gender.FEMALE).enabled(true).build();
             dao.insertUser(user);
-            User user2 = User.builder().id(5654231).username("username").password("password").name("name").email("email").dob(LocalDate.parse("2020-04-11")).gender(User.Gender.MALE).enabled(true);
+            User user2 = User.builder().id((long) 5654231).username("username").password("password").name("name").email("email").dob(LocalDate.parse("2020-04-11")).gender(User.Gender.MALE).enabled(true).build();
             dao.insertUser(user2);
-            dao.findByID((long) 5654231)
+            dao.findByID((long) 5654231);
             dao.findByUsername("imdal");
             dao.delete(user2);
             dao.list().stream().forEach(System.out::println);
--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,5 +1,7 @@
 package user;
 import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Data;
 
 @Builder
 @Data
